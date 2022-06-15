abc39787a38abf1da8718f454f33d051e825a041
AnousoneM <anousone.mounivongs@gmail.com>
2022-06-15 15:58:02 +0200

    mise à jour du contrôle de la regex

--- controller/form-controller.php
+++ controller/form-controller.php
@@ -6,7 +6,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // je déclare un tableau d'erreurs vide pour stocker toutes mes erreurs
     $errors = [];
 
-    // Contrôle du champ lastname
+    // je déclare mes regex 
+    $regexName = "/^[a-zA-Z]+$/";
+
+    ////////////////////////////////
+    // Contrôle du champ lastname //
+    ////////////////////////////////
+
     // 2 - isset permet de vérifier que l'input lastname existe dans la superglobale POST
     if (isset($_POST['lastname'])) {
 
@@ -14,6 +20,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if ($_POST['lastname'] == '') {
             // si c vide, nous allons creer un clef dans le tableau d'erreurs
             $errors['lastname'] = "Champ obligatoire";
+            // Nous utilison la fonction preg_match pour valider le format
+        } else if (!preg_match($regexName, $_POST['lastname'])) {
+            // si mauvais format, message d'erreur
+            $errors['lastname'] = "Format invalide";
         }
     }
 }
